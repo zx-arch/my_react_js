5954b95357a0f5f77ab0896bc16c735cfcc08aa0
renaldikristian <renaldi.kristian@ti.ukdw.ac.id>
2023-10-21 12:44:04 +0700

    Menambahkan Komentar pada Detail Film dan Atur Responsive

--- project_film/src/App.js
+++ project_film/src/App.js
@@ -2,11 +2,35 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import ReactPaginate from 'react-paginate';
 
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-modal-button" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+
+  );
+};
+
 function App() {
   const [movie, setMovie] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [showPrevious, setShowPrevious] = useState(false);
   const [showNext, setshowNext] = useState(false);
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+  const [names, setNames] = useState([]);
+  const [name, setName] = useState('');
+  const [showForm, setShowForm] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const fetchMovie = async () => {
     console.log(movie);
@@ -17,6 +41,14 @@ function App() {
   useEffect(() => {
     fetchMovie()
   }, [])
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   const search = async (q) => {
     const URL = `https://api.themoviedb.org/3/search/movie?query=${q}&api_key=27bd4dad0754c77391111e35f827bd6a`;
     const response = await fetch(URL);
@@ -27,11 +59,23 @@ function App() {
     }
   };
 
+  const handleCommentSubmit = () => {
+    if (name && comment) {
+      setNames([...names, name]);
+      setComments([...comments, comment]);
+      setName(''); // Clear the name input
+      setComment(''); // Clear the comment input
+    }
+    setShowForm(false);
+  };
+
+
   const detail = async (id) => {
     const URL = `https://api.themoviedb.org/3/movie/${id}?api_key=27bd4dad0754c77391111e35f827bd6a`;
     const response = await fetch(URL);
     const data = await response.json();
     setSelectedMovie(data);
+    setShowForm(false);
   }
 
   const handlePage = async (page) => {
@@ -55,16 +99,18 @@ function App() {
       }
     }
 
+    setShowForm(true);
   }
 
   const refreshPage = async () => {
-    window.location.reload(false);
+    window.location = document.referrer;
   }
   return (
     <>
       <form
         action=""
         className="search-bar"
+        style={{ display: showForm ? '' : 'none' }}
         onSubmit={(e) => {
           e.preventDefault();
           const searchValue = e.target.querySelector('.MovieSearch').value;
@@ -108,10 +154,57 @@ function App() {
                 <p>Release Date : {selectedMovie.release_date}</p>
                 <p>Duration: {(selectedMovie.runtime - (selectedMovie.runtime % 60)) / 60} Jam {selectedMovie.runtime % 60} Menit</p>
                 <p>Status: {selectedMovie.status}</p>
-                <p>Tagline: {selectedMovie.tagline}</p>
+                <p>Rating: {selectedMovie.vote_average}</p>
                 <button className='button-5' onClick={refreshPage}>Kembali</button>
+
+                {comments.length > 0 && (
+                  <button className="open-modal-button modal-button" onClick={handleOpenModal}>
+                    Lihat Komentar
+                  </button>
+                )}
+
+                <input
+                  className="input-name"
+                  type="text"
+                  id="nama"
+                  placeholder="Masukkan nama"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+
+                <div className="comment-container">
+                  <textarea
+                    className="comment-box"
+                    placeholder="Tambahkan komentar Anda..."
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                  ></textarea>
+                  <button className="submit-button" onClick={handleCommentSubmit}>
+                    Submit
+                  </button>
+
+                </div>
+
+
               </main>
+
             </div>
+
+            <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+              {comments.length > 0 && (
+                <div className="comments">
+                  <h3>Isi komentar</h3>
+                  {comments.map((comment, index) => (
+                    <div key={index} className="comment">
+                      <p>
+                        <strong>{names[index]}:</strong> {comment}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </Modal>
+
             {/* <h2>{selectedMovie.title}</h2>
             <p>{selectedMovie.overview}</p> */}
             {/* Tampilkan data film lainnya sesuai kebutuhan */}
